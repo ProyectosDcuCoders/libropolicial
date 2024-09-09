26f84c74af7f545253986ea3892859c87c049aef
Cristian <retacris30@gmail.com>
2024-09-09 08:15:46 -0300

    full actualizacion 09-09-24

--- Libropolicial/templates/temporales/models.py
+++ Libropolicial/templates/temporales/models.py
@@ -79,10 +79,8 @@ class ServiciosEmergencia(models.Model):
 class InstitucionesFederales(models.Model):
     nombre =  models.CharField(max_length=60)
      
-    def __str__(self):
-        return self.nombre
-            
-            
+    def __srt__(self):
+        return self.nombre             
 
 # Clase abstracta que sirve como base para todas las comisarías.
 class BaseComisaria(models.Model):
@@ -91,7 +89,7 @@ class BaseComisaria(models.Model):
     codigo = models.ForeignKey(CodigoPolicialUSH, null=True, blank=True, on_delete=models.SET_NULL)  # Relaciona la comisaría con un código policial; si el código se elimina, se establece a NULL.
     solicitante_codigo = models.ForeignKey(SolicitanteCodigo, null=True, blank=True, on_delete=models.SET_NULL)  # Relaciona la comisaría con un solicitante de código; si el solicitante se elimina, se establece a NULL.
     codigos_secundarios = models.ManyToManyField(CodigosSecundarios, blank=True)  # Relaciona la comisaría con múltiples códigos secundarios, permitiendo que el campo quede vacío.
-   # dependencias_secundarias = models.ManyToManyField(DependenciasSecundarias, blank=True)  # Relaciona la comisaría con múltiples dependencias secundarias, permitiendo que el campo quede vacío.
+    dependencias_secundarias = models.ManyToManyField(DependenciasSecundarias, blank=True)  # Relaciona la comisaría con múltiples dependencias secundarias, permitiendo que el campo quede vacío.
     movil_patrulla = models.CharField(max_length=255, null=True, blank=True)  # Campo para almacenar información sobre el móvil de patrulla, permitiendo que quede vacío.
     a_cargo = models.CharField(max_length=255, null=True, blank=True)  # Campo para almacenar el nombre de la persona a cargo, permitiendo que quede vacío.
     secundante = models.CharField(max_length=255, null=True, blank=True)  # Campo para almacenar el nombre del secundante, permitiendo que quede vacío.
@@ -114,7 +112,7 @@ class BaseComisaria(models.Model):
     dependencias_provinciales = models.ManyToManyField(DependenciasProvinciales, blank=True)  # Relaciona la comisaría con múltiples dependencias provinciales, permitiendo que el campo quede vacío.
     servicios_emergencia = models.ManyToManyField(ServiciosEmergencia, blank=True)  # Relaciona la comisaría con múltiples servicios de emergencia, permitiendo que el campo quede vacío.
     instituciones_federales = models.ManyToManyField(InstitucionesFederales, blank=True)  # Relaciona la comisaría con múltiples servicios de insdtituciones federales , permitiendo que el campo quede vacío.
-    dependendencias_secundarias = models.ManyToManyField(DependenciasSecundarias, blank=True)  # Relaciona la comisaría con múltiples servicios de dependencias secundarias , permitiendo que el campo quede vacío.
+
 
 
     class Meta:
@@ -151,9 +149,6 @@ class DetalleServicioEmergencia(models.Model):
     numero_movil_bomberos = models.CharField(max_length=20, null=True, blank=True)  # Campo para almacenar el número de móvil de los bomberos, permitiendo que quede vacío.
     nombre_a_cargo_bomberos = models.CharField(max_length=255, null=True, blank=True)  # Campo para almacenar el nombre de la persona a cargo de los bomberos, permitiendo que quede vacío.
 
-    def __str__(self):
-        return f"Servicio de Emergencia: {self.servicio_emergencia.nombre}"
-
 # Clase para manejar los detalles adicionales de Instituciones Hospitalarias.
 class DetalleInstitucionHospitalaria(models.Model):
     institucion_hospitalaria = models.ForeignKey(InstitucionesHospitalarias, on_delete=models.CASCADE)  # Relaciona el detalle con una institución hospitalaria; si la institución se elimina, también se elimina el detalle.
@@ -164,9 +159,6 @@ class DetalleInstitucionHospitalaria(models.Model):
     comisaria_quinta = models.ForeignKey(ComisariaQuinta, null=True, blank=True, on_delete=models.CASCADE, related_name='detalles_institucion_hospitalaria')  # Relaciona el detalle con la Comisaría Quinta, permitiendo que quede vacío; si la comisaría se elimina, también se elimina el detalle.
     numero_movil_hospital = models.CharField(max_length=20, null=True, blank=True)  # Campo para almacenar el número de móvil del hospital, permitiendo que quede vacío.
     nombre_a_cargo_hospital = models.CharField(max_length=255, null=True, blank=True)  # Campo para almacenar el nombre de la persona a cargo del hospital, permitiendo que quede vacío.
-
-    def __str__(self):
-        return f"Institución Hospitalaria: {self.institucion_hospitalaria.nombre}"
 
 # Clase para manejar los detalles adicionales de Dependencias Municipales.
 class DetalleDependenciaMunicipal(models.Model):
@@ -179,9 +171,6 @@ class DetalleDependenciaMunicipal(models.Model):
     numero_movil_municipal = models.CharField(max_length=20, null=True, blank=True)  # Campo para almacenar el número de móvil de la dependencia municipal, permitiendo que quede vacío.
     nombre_a_cargo_municipal = models.CharField(max_length=255, null=True, blank=True)  # Campo para almacenar el nombre de la persona a cargo de la dependencia municipal, permitiendo que quede vacío.
 
-    def __str__(self):
-        return f"Dependencia Municipal: {self.dependencia_municipal.nombre}"
-
 # Clase para manejar los detalles adicionales de Dependencias Provinciales.
 class DetalleDependenciaProvincial(models.Model):
     dependencia_provincial = models.ForeignKey(DependenciasProvinciales, on_delete=models.CASCADE)  # Relaciona el detalle con una dependencia provincial; si la dependencia se elimina, también se elimina el detalle.
@@ -193,31 +182,13 @@ class DetalleDependenciaProvincial(models.Model):
     numero_movil_provincial = models.CharField(max_length=20, null=True, blank=True)  # Campo para almacenar el número de móvil de la dependencia provincial, permitiendo que quede vacío.
     nombre_a_cargo_provincial = models.CharField(max_length=255, null=True, blank=True)  # Campo para almacenar el nombre de la persona a cargo de la dependencia provincial, permitiendo que quede vacío.
 
-    def __str__(self):
-        return f"Dependencia Provincial: {self.dependencia_provincial.nombre}"
-
+# Clase para manejar los detalles adicionales de Dependencias Provinciales.
 class DetalleInstitucionFederal(models.Model):
-    institucion_federal = models.ForeignKey(InstitucionesFederales, on_delete=models.CASCADE)
-    comisaria_primera = models.ForeignKey(ComisariaPrimera, null=True, blank=True, on_delete=models.CASCADE, related_name='detalles_institucion_federal')
-    comisaria_segunda = models.ForeignKey(ComisariaSegunda, null=True, blank=True, on_delete=models.CASCADE, related_name='detalles_institucion_federal')
-    comisaria_tercera = models.ForeignKey(ComisariaTercera, null=True, blank=True, on_delete=models.CASCADE, related_name='detalles_institucion_federal')
-    comisaria_cuarta = models.ForeignKey(ComisariaCuarta, null=True, blank=True, on_delete=models.CASCADE, related_name='detalles_institucion_federal')
-    comisaria_quinta = models.ForeignKey(ComisariaQuinta, null=True, blank=True, on_delete=models.CASCADE, related_name='detalles_institucion_federal')   
-    numero_movil_federal = models.CharField(max_length=20, null=True, blank=True)
-    nombre_a_cargo_federal = models.CharField(max_length=255, null=True, blank=True)
-
-    def __str__(self):
-        return f"Institución Federal: {self.institucion_federal.nombre}"
-
-class DetalleDependenciaSecundaria(models.Model):
-    dependencia_secundaria = models.ForeignKey(DependenciasSecundarias, on_delete=models.CASCADE)
-    comisaria_primera = models.ForeignKey(ComisariaPrimera, null=True, blank=True, on_delete=models.CASCADE, related_name='detalles_dependencia_secundaria')
-    comisaria_segunda = models.ForeignKey(ComisariaSegunda, null=True, blank=True, on_delete=models.CASCADE, related_name='detalles_dependencia_secundaria')
-    comisaria_tercera = models.ForeignKey(ComisariaTercera, null=True, blank=True, on_delete=models.CASCADE, related_name='detalles_dependencia_secundaria')
-    comisaria_cuarta = models.ForeignKey(ComisariaCuarta, null=True, blank=True, on_delete=models.CASCADE, related_name='detalles_dependencia_secundaria')
-    comisaria_quinta = models.ForeignKey(ComisariaQuinta, null=True, blank=True, on_delete=models.CASCADE, related_name='detalles_dependencia_secundaria')    
-    numero_movil_secundaria = models.CharField(max_length=20, null=True, blank=True)
-    nombre_a_cargo_secundaria = models.CharField(max_length=255, null=True, blank=True)
-
-    def __str__(self):
-        return f"Dependencia Secundaria: {self.dependencia_secundaria.dependencia}"
+    institucion_federal = models.ForeignKey(InstitucionesFederales, on_delete=models.CASCADE)  # Relaciona el detalle con una instsitucion federal; si la dependencia se elimina, también se elimina el detalle.
+    comisaria_primera = models.ForeignKey(ComisariaPrimera, null=True, blank=True, on_delete=models.CASCADE, related_name='detalles_institucion_federal')  # Relaciona el detalle con la Comisaría Primera, permitiendo que quede vacío; si la comisaría se elimina, también se elimina el detalle.
+    comisaria_segunda = models.ForeignKey(ComisariaSegunda, null=True, blank=True, on_delete=models.CASCADE, related_name='detalles_institucion_federal')  # Relaciona el detalle con la Comisaría Segunda, permitiendo que quede vacío; si la comisaría se elimina, también se elimina el detalle.
+    comisaria_tercera = models.ForeignKey(ComisariaTercera, null=True, blank=True, on_delete=models.CASCADE, related_name='detalles_institucion_federal')  # Relaciona el detalle con la Comisaría Tercera, permitiendo que quede vacío; si la comisaría se elimina, también se elimina el detalle.
+    comisaria_cuarta = models.ForeignKey(ComisariaCuarta, null=True, blank=True, on_delete=models.CASCADE, related_name='detalles_institucion_federal')  # Relaciona el detalle con la Comisaría Cuarta, permitiendo que quede vacío; si la comisaría se elimina, también se elimina el detalle.
+    comisaria_quinta = models.ForeignKey(ComisariaQuinta, null=True, blank=True, on_delete=models.CASCADE, related_name='detalles_institucion_federal')  # Relaciona el detalle con la Comisaría Quinta, permitiendo que quede vacío; si la comisaría se elimina, también se elimina el detalle.
+    numero_movil_federal = models.CharField(max_length=20, null=True, blank=True)  # Campo para almacenar el número de móvil de la dependencia provincial, permitiendo que quede vacío.
+    nombre_a_cargo_federal = models.CharField(max_length=255, null=True, blank=True)  # Campo para almacenar el nombre de la persona a cargo de la dependencia provincial, permitiendo que quede vacío.
