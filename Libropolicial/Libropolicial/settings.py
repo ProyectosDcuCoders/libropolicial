2d746eb36af794ac2200774bd1ef69310642e5dd
Cristian <retacris30@gmail.com>
2024-11-14 08:13:13 -0300

    cambios

--- Libropolicial/Libropolicial/settings.py
+++ Libropolicial/Libropolicial/settings.py
@@ -87,6 +87,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'compartido.context_processors.global_user_permissions',# agreege este nuevo para permisos globales dcu101
             ],
         },
     },
